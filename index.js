58b83905afccf5da5cd818781ebc1367660af9bb
pandaleex <108262104+pandaleex@users.noreply.github.com>
2023-05-26 17:55:00 +1000

    testing

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const Users = Models.User;
 //   useUnifiedTopology: true,
 // });
 
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => { console.log('Connected to MongoDB'); }) .catch((err) => { console.error(err); });
 
 const app = express();
 const cors = require("cors");
@@ -30,7 +31,7 @@ app.use(
       if (allowedOrigins.indexOf(origin) === -1) {
         // If a specific origin isn’t found on the list of allowed origins
         let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
+          "The CORS policy for this application dont allow access from origin " +
           origin;
         return callback(new Error(message), false);
       }
