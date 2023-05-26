5e3ca6fc5e780ac8e42643b729f94f2457d08651
pandaleex <108262104+pandaleex@users.noreply.github.com>
2023-05-26 15:55:26 +1000

    Connecting database to API

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ const { check, validationResult } = require("express-validator");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect("mongodb://localhost:27017/myFlix", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://localhost:27017/myFlix", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
 const cors = require("cors");
 //app.use(cors());
