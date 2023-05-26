a7dcd9657414d53deb7328f0c3a5719fb3c19efa
pandaleex <108262104+pandaleex@users.noreply.github.com>
2023-05-26 16:53:33 +1000

    Fixing heroku app error

--- index.js
+++ index.js
@@ -312,7 +312,7 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
+app.listen(port,() => {
  console.log('Listening on Port ' + port);
 });
 
